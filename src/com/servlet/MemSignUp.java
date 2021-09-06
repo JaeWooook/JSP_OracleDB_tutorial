6e553cd54e4761a072435dd3a71aaf9c0e1a4b3b
JaeWook Jeong <br12345678@ajou.ac.kr>
2021-09-07 00:13:57 +0900

    jsp script example

--- src/com/servlet/MemSignUp.java
+++ src/com/servlet/MemSignUp.java
@@ -16,7 +16,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/mSignUp")
 public class MemSignUp extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
     /**
      * @see HttpServlet#HttpServlet()
      */
